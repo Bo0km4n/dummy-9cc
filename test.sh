c7a8475f32b6dc96fd0d7e251df37ffc4108cedc
Bo0km4n <pokopenkatuya@gmail.com>
2018-10-14 21:29:33 +0900

    Add '+' and '-' operator refs: 916237396164ae25557b62bf0d1ca9d9cf8c2070

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 try() {
   expected="$1"
@@ -9,13 +9,16 @@ try() {
   ./tmp
   actual="$?"
 
-  if [ ${actual} != ${expected} ]; then
-      echo "$input expected, but got $actual"
-      exit 1
+  if [ "${actual}" = "${expected}" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input expected, but got $actual"
+    exit 1
   fi
+    
 }
 
 try 0 0 
 try 42 42
-
+try 21 '5+20-4'
 echo ok
