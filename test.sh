bc860f390ce395da5c33e9b979bfb280aae51fb7
Bo0km4n <pokopenkatuya@gmail.com>
2019-04-21 17:41:26 +0900

    fix test

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ try() {
 
 try 0 "return 0;"
 try 42 "return 42;"
-try 21 "return 21;"
+try 21 "1+2; return 5+20-4;"
 try 41 'return 12 + 34 - 5;'
 try 153 'return 1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17;'
 try 10  'return 2*3+4;'
